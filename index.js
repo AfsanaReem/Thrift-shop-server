1f7163e67ae078a1a9247db6a28868f480815639
Reem <apple@Apples-MacBook-Pro.local>
2022-11-27 23:29:10 +0600

    mongodb added

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const stripe = require("stripe")(process.env.STRIPE_SK);
+require('dotenv').config();
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.khy11rb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.log);
+app.get('/', (req, res) => {
+    res.send('server is running')
+})
+app.listen(port, () => console.log(port));
